d8d9771094d25b71d064baaf858df09e20a75fd8
Leon Xu <leonxu@berkeley.edu>
2018-11-10 15:07:00 -0800

    add rowdy group generation

--- deliverable1/inputs/small/small-generator.py
+++ deliverable1/inputs/small/small-generator.py
@@ -6,6 +6,7 @@ INPUT_SIZE = 'small'
 BUS_COUNT = 3
 BUS_SIZE = 10
 ROWDY_GROUPS_MAX = 100
+MIN_ROWDY_GROUP_SIZE = 5
 STUDENT_COUNT = BUS_COUNT * BUS_SIZE
 EXTRA_CONNECTIONS = STUDENT_COUNT
 
@@ -23,8 +24,6 @@ for bus_num in range(BUS_COUNT):
     first = BUS_SIZE * bus_num;
     buses.append([students[first + i] for i in range(BUS_SIZE)])
 
-for bus in buses:
-    print(len(bus))
 # Fully connect these groups
 for bus in buses:
     for first_student_index in range(BUS_SIZE):
@@ -32,13 +31,13 @@ for bus in buses:
             G.add_edge(bus[first_student_index], bus[second_student_index])
 
 # Place these groups in output
-output_file = open('../../outputs/' + INPUT_SIZE + '/small.out', 'w')
+output_file = open('../../outputs/' + INPUT_SIZE + '/' + INPUT_SIZE + '.out', 'w')
 for bus in buses:
     output_file.write(str([str(i) for i in bus]) + '\n')
 
-# Draw solution in graph.png
+# Draw solution in solution.png
 # nx.draw(G)
-# plt.savefig('graph.png')
+# plt.savefig('solution.png')
 
 # Add EXTRA_CONNECTIONS number of extra connections
 for i in range(EXTRA_CONNECTIONS):
@@ -48,35 +47,35 @@ for i in range(EXTRA_CONNECTIONS):
     student_2 = buses[random_buses[1]][random_indices[1]]
     G.add_edge(student_1, student_2)
 
-# Draw graph of student connections in graph.png
-nx.draw(G)
-plt.savefig('graph.png')
+# Draw graph of student connections in connections.png
+# nx.draw(G)
+# plt.savefig('connections.png')
 
 # Write graph of student connections to graph.gml
 nx.write_gml(G, 'graph.gml', nx.readwrite.gml.literal_stringizer)
 
 
-# param_file = open('parameters.txt', 'w')
-# rowdies_graph = nx.Graph()
-#
-# param_file.write(str(BUS_COUNT) + '\n')
-# param_file.write(str(BUS_SIZE) + '\n')
-#
-# #Need to have random rowdy pairs not contradict solution set
-#
-# # Random rowdy pairs
-# rowdies = random.sample(range(1, (int) (STUDENT_COUNT * (STUDENT_COUNT - 1) / 2)), ROWDY_GROUPS_MAX)
-# for i in rowdies:
-#     u = 1
-#     counter = 0
-#     while (counter < i):
-#         counter += STUDENT_COUNT - u
-#         u += 1
-#     v = STUDENT_COUNT - (counter - i)
-#     param_file.write('[\'' + str(u) + '\', \'' + str(v) + '\'] \n')
-#     rowdies_graph.add_edge(u, v)
-#
-#
+param_file = open('parameters.txt', 'w')
+rowdies_graph = nx.Graph()
+
+param_file.write(str(BUS_COUNT) + '\n')
+param_file.write(str(BUS_SIZE) + '\n')
+
+# Rowdy group generation
+rowdy_groups = []
+for group in range(ROWDY_GROUPS_MAX):
+    group_size = random.choice(range(MIN_ROWDY_GROUP_SIZE, BUS_SIZE))
+    random_buses = random.sample(range(BUS_COUNT), 2)
+    random_indices = random.sample(range(BUS_SIZE), 2)
+    student_1 = buses[random_buses[0]][random_indices[0]]
+    student_2 = buses[random_buses[1]][random_indices[1]]
+    remaining = [elem for elem in range(STUDENT_COUNT) if elem != student_1 and elem != student_2]
+    sample_remaining = random.sample(remaining, group_size - 2)
+    rowdy_groups.append([student_1] + [student_2] + sample_remaining)
+
+# Write rowdy groups to parameters.txt
+for group in rowdy_groups:
+    param_file.write(str([str(i) for i in group]) + '\n')
 
 # Draw rowdy groups in graph.png
 # nx.draw(rowdies_graph)
